ee71372749770484f1b5a1f8b2e0253e7364776d
kobe <kobecode24@outlook.com>
2025-03-12 11:03:09 +0000

    Add Builder Pattern to ApiResponse DTO
    - Introduce @Builder annotation to ApiResponse class
    - Enable fluent and flexible object creation for API responses
    - Simplify instantiation of ApiResponse objects with builder methods

--- src/main/java/org/doctech/common/dto/ApiResponse.java
+++ src/main/java/org/doctech/common/dto/ApiResponse.java
@@ -1,12 +1,14 @@
 package org.doctech.common.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ApiResponse {
     private boolean success;
     private String message;
