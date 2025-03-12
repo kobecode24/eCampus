aefc6d096c8da23f9171eb25f809545a1b0bbcaf
kobe <kobecode24@outlook.com>
2025-03-12 11:01:15 +0000

    Add ComponentScan Annotation to DocTechApplication
    - Include @ComponentScan annotation in main application class
    - Ensure proper component scanning for Spring Boot application

--- src/main/java/org/doctech/DocTechApplication.java
+++ src/main/java/org/doctech/DocTechApplication.java
@@ -1,8 +1,8 @@
 package org.doctech;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DocTechApplication {
