017b06e14656b0af081070e9731a6ba22c29d725
kobe <kobecode24@outlook.com>
2025-03-24 10:07:37 +0000

    Add new TechnologyType enum values for documentation categorization
    - Introduce GETTING_STARTED, RESOURCES, and ADVANCED to the TechnologyType enum to enhance the classification of documentation types.

--- src/main/java/org/doctech/documentation/model/TechnologyType.java
+++ src/main/java/org/doctech/documentation/model/TechnologyType.java
@@ -1,11 +1,14 @@
 package org.doctech.documentation.model;
 
 public enum TechnologyType {
+    GETTING_STARTED,
     PROGRAMMING_LANGUAGE,
     FRAMEWORK,
     LIBRARY,
     FRONTEND,
     BACKEND,
+    RESOURCES,
+    ADVANCED,
     DEVOPS,
     API,
     TOOL,
